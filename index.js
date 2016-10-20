ab8d0c68ac2c06d8eb7312fe321d0233dd5982c2
Hack Reactor Students <accounts@hackreactor.com>
2016-10-19 17:03:47 -0700

    corrected json && index.js for heroku

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('word')
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/../client/'));
+
+app.listen(3000, function () {
+  console.log('listening on port 3000');
+});
