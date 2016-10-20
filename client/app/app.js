9da3c54b12766bc0f85edc69392e42ce7a6bff9c
Hack Reactor Students <accounts@hackreactor.com>
2016-10-20 15:14:48 -0700

    Working default news section, installed angular dependencies for material

---
+++ client/app/app.js
@@ -0,0 +1,22 @@
+angular.module('inews', [
+  'ngMaterial',
+  'inews.services',
+  'inews.defaultNews'
+])
+.config(function($httpProvider) {
+  $httpProvider.interceptors.push('AttachToken');
+})
+.factory('AttachToken', function($window) {
+  var attach = {
+    request: function(object) {
+      var jwt = $window.localStorage.getItem('com.inews');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;
+      } 
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object
+    }
+  }
+
+  return attach;
+});
---
+++ client/app/services/services.js
@@ -0,0 +1,40 @@
+angular.module('inews.services', [])
+
+.factory('News', function($http) {
+
+  // var getCurrentLocation = function(lat, long) {
+  //   var url = 'http://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + long ;
+  //   return $http({
+  //     method: 'GET',
+  //     url: url
+  //   })
+  //   .then(function (resp) {
+  //     if (resp) {
+  //       var address = resp.body.address;
+  //       return {
+  //         neigbourhood: address.neighbourhood,
+  //         city: address.city,
+  //         state: address.state,
+  //         country: address.country_code
+  //       }
+  //     }
+  //   });
+  // };
+
+  // var getLocalNews = function(locationInfo) {
+
+  // };
+
+  var getDefaultNews = function(src) {
+    return $http({
+      method: 'GET',
+      url: 'https://newsapi.org/v1/articles?source=' + src + '&apiKey=25e1cb57f180459c8a21c273e7f6a795'
+    })
+    .then(function(data) {
+      return data;
+    });
+  }
+  return {
+    getDefaultNews: getDefaultNews
+  }
+});
